2a874c4daec52d4fa6b0fd2f657eff64038f5b5a
LIm-JY <63032446+LIm-JY@users.noreply.github.com>
2023-02-14 17:18:50 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 1;
+        if(Math.sqrt(n)-(int)(Math.sqrt(n))>0) {
+        	return 2;
+        }
+        return answer;
+    }
+}
