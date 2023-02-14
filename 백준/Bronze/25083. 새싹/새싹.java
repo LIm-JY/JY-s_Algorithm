ce4baaa9a5f31c8f3c9c94fdd6feec3467a8c36a
LIm-JY <63032446+LIm-JY@users.noreply.github.com>
2023-02-14 16:17:46 +0900

    [Bronze V] Title: 새싹, Time: 76 ms, Memory: 11240 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		// 새싹을 출력한다.
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+	}
+}
