e99957d480620c2ab3b4691fd35cea6e2265b126
LIm-JY <63032446+LIm-JY@users.noreply.github.com>
2023-02-14 16:15:56 +0900

    [Bronze V] Title: ??!, Time: 76 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 선언
+		StringTokenizer st = new StringTokenizer(br.readLine()  , " ");
+		String str = st.nextToken();
+		// 첫째 줄에 준하의 놀람을 출력한다. 놀람은 아이디 뒤에 ??!를 붙여서 나타낸다.
+		System.out.print(str+"??!");
+	}
+}
