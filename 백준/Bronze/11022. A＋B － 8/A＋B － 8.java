94e52ccad44af8fbd6a257d7646699cd5439d2fd
LIm-JY <63032446+LIm-JY@users.noreply.github.com>
2023-02-14 16:16:36 +0900

    [Bronze V] Title: A+B - 8, Time: 84 ms, Memory: 11720 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 선언
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); // 선언
+		int T = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    		int A = Integer.parseInt(st.nextToken());
+    		int B = Integer.parseInt(st.nextToken());
+    		int res = A+B;
+    		if(i < T) {
+    			bw.write(String.valueOf("Case #"+i+": "+A+" + "+B+" = "+res));
+    			bw.newLine(); // 줄바꿈
+    		}else {
+    			bw.write(String.valueOf("Case #"+i+": "+A+" + "+B+" = "+res));
+    		}
+		}
+		bw.flush(); // 남아있는 데이터 모두 출력
+	}
+}
