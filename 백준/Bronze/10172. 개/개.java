4ddc416bab382c4298ad9ba151c44303b6ebe58f
LIm-JY <63032446+LIm-JY@users.noreply.github.com>
2023-02-14 16:14:34 +0900

    [Bronze V] Title: 개, Time: 72 ms, Memory: 11264 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 선언
+		
+		// 고양이를 출력한다.
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+	}
+}
