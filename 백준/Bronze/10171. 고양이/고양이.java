3a8969326a944022bdeb16d26da3a967bcace498
LIm-JY <63032446+LIm-JY@users.noreply.github.com>
2023-02-14 16:14:24 +0900

    [Bronze V] Title: 고양이, Time: 72 ms, Memory: 11304 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 선언
+		
+		// 고양이를 출력한다.
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
