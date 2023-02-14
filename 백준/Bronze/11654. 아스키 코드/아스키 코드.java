09b4a4fa02470ba3bfeae64103889c9b95b7bcd9
LIm-JY <63032446+LIm-JY@users.noreply.github.com>
2023-02-14 16:17:04 +0900

    [Bronze V] Title: 아스키 코드, Time: 92 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 선언
+		System.out.println((int)br.readLine().charAt(0));
+	}
+}
