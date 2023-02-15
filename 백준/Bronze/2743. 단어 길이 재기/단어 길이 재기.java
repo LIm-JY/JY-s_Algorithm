900997cfd9cd2ee583c9e87cb5d238f4cba23ae9
LIm-JY <63032446+LIm-JY@users.noreply.github.com>
2023-02-15 09:12:03 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 76 ms, Memory: 11552 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 선언
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); // 선언
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+	    String s = st.nextToken();
+	    bw.write(Integer.toString(s.length()));
+	    bw.flush();
+	}
+}
