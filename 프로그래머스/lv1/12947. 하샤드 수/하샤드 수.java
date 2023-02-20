65bad15e85822b3552b0fa8d93720c2d437444d2
LIm-JY <63032446+LIm-JY@users.noreply.github.com>
2023-02-20 11:05:47 +0900

    [level 1] Title: 하샤드 수, Time: 0.14 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        String[] arr = Integer.toString(x).split("");
+        int a = 0;
+        for (int i = 0; i < arr.length; i++) {
+			a += Integer.parseInt(arr[i]);
+		}
+        if (x%a!=0) {
+        	answer = false;
+		}
+        return answer;
+    }
+}
